7cee3d6454feab6606e88428dd330102d2d2ffda
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-24 17:27:10 +0000

    working project used two different components for diffrent currencies

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-ReactDOM.render(<React.StrictMode><Form /></React.StrictMode>, document.getElementById("root"));
+const Dollars = props => {
+    const value = (props.amount * props.ratio).toFixed(2);
+    return (
+        <div>Amount in $: {props.amount > 0 ? value : ""}  </div>)
+}
+
+const Euros = props => {
+    const value = (props.amount * props.ratio).toFixed(2);
+    return (
+        <div>Amount in €: { props.amount > 0 ? value : ""} </div>)
+}
+
+
+class CurrencyConverter extends React.Component {
+    state = {
+        amount: 0,
+        ratioDollar: 1.34,
+        ratioEuro: 1.19,
+    }
+    handleChange = e => {
+        this.setState({
+            amount: e.target.value,
+        })
+    }
+    render() {
+        const {amount,ratioDollar,ratioEuro} = this.state;
+        return (
+            <>
+                <label>
+                    <input
+                        type="number"
+                        value={amount}
+                        onChange={this.handleChange}
+                    />
+                    <Dollars ratio={ratioDollar} amount={amount} />
+                    <Euros ratio={ratioEuro}  amount={amount} />
+                </label>
+            </>
+        )
+    }
+}
+
+
+
+
+
+
+ReactDOM.render(<React.StrictMode><CurrencyConverter /></React.StrictMode>, document.getElementById("root"));
