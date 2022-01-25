097546faaf9914d8e3589a6dc3413412f11e339f
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-25 22:03:47 +0000

    upgrading for product selection

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ const Currency = props => {
 class CurrencyConverter extends React.Component {
     state = {
         amount: 0,
+        product: "petrol",
+
     }
 
-    currencies =[
+static defaultProps = {
+    currencies : [
         {
             id: 1,
             name: "dollar: ",
@@ -28,30 +31,56 @@ class CurrencyConverter extends React.Component {
             name: "zloty: ",
             ratio: 4.75,
         },
-    ];
+    ]
+
+}
+
+   
+    insertSuffix = (select) => {
+        if(select === "electricity") return  <em>kWh</em>
+        else if(select === "petrol") return <em>litres</em>
+        else if(select === "oranges") return <em>kg</em>
+        else{
+            return null;
+        }
+    }
 
     handleChange = e => {
         this.setState({
             amount: e.target.value,
         })
     }
+    handleSelect = e => {
+        this.setState({
+            product: e.target.value,
+        })
+    }
     render() {
         const amount = this.state.amount;
-        const calculators = this.currencies.map( c => 
+        const calculators = defaultProps.currencies.map( c => 
             (
                 <Currency key={c.id} ratio={c.ratio} amount={amount} name={c.name} />
             ))
         return (
-            <>
-                <label>
-                    <input
+        <>
+            <label style={{fontWeight: "bold"}}>Choose product   
+                <select onChange={this.handleSelect} style={{marginLeft: "10px"}}>
+                    <option value="electricity">electricity</option>
+                    <option value="petrol">petrol</option>
+                    <option value="oranges">oranges</option>
+                </select>
+            </label>
+            <br />
+            <label>
+                <input style={{marginTop: "20px"}}
                         type="number"
                         value={amount}
                         onChange={this.handleChange}
-                    />
+                />
+                {this.insertSuffix(this.state.product)}
                 {calculators}
-                </label>
-            </>
+            </label>
+        </>
         )
     }
 }
