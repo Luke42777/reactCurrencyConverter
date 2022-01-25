d18e71bb126f62330f6b54cc708afce9207cb5ee
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-25 20:32:43 +0000

    upgrade method map used

--- app.js
+++ app.js
@@ -3,24 +3,44 @@ const Currency = props => {
     const value = (props.amount * props.ratio).toFixed(2);
 
         return (
-            <div>{props.title}{ props.amount > 0 ? value : ""} </div>)
+            <div>{props.name}{ props.amount > 0 ? value : ""} </div>)
 }
-
 
 
 class CurrencyConverter extends React.Component {
     state = {
         amount: 0,
-        ratioDollar: 1.34,
-        ratioEuro: 1.19,
     }
+
+    currencies =[
+        {
+            id: 1,
+            name: "dollar: ",
+            ratio: 1.34,
+        },
+        {
+            id: 2,
+            name: "euro: ",
+            ratio: 1.19,
+        },
+        {
+            id: 3,
+            name: "zloty: ",
+            ratio: 4.75,
+        },
+    ];
+
     handleChange = e => {
         this.setState({
             amount: e.target.value,
         })
     }
     render() {
-        const {amount,ratioDollar,ratioEuro} = this.state;
+        const amount = this.state.amount;
+        const calculators = this.currencies.map( c => 
+            (
+                <Currency key={c.id} ratio={c.ratio} amount={amount} name={c.name} />
+            ))
         return (
             <>
                 <label>
@@ -29,8 +49,7 @@ class CurrencyConverter extends React.Component {
                         value={amount}
                         onChange={this.handleChange}
                     />
-                    <Currency ratio={ratioDollar} amount={amount} title="Amount in dollar: " />
-                    <Currency ratio={ratioEuro}  amount={amount}  title="Amount in euro: " />
+                {calculators}
                 </label>
             </>
         )
@@ -42,4 +61,4 @@ class CurrencyConverter extends React.Component {
 
 
 
-ReactDOM.render(<React.StrictMode><CurrencyConverter /></React.StrictMode>, document.getElementById("root"));
+ReactDOM.render(<React.StrictMode><CurrencyConverter /></React.StrictMode>, document.getElementById("root"))
